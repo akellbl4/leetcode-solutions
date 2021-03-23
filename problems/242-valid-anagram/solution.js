2f9a392e5d547336b9c40f3b44bacad4d4f74928
Pavel Mineev <pavel@mineev.me>
2021-03-23 14:26:52 +0300

    fix formatting

--- problems/242-valid-anagram/solution.js
+++ problems/242-valid-anagram/solution.js
@@ -4,37 +4,37 @@
  * @return {boolean}
  */
 export function isAnagram(s, t) {
-	if (s.length !== t.length) {
-		return false;
-	}
+  if (s.length !== t.length) {
+    return false;
+  }
 
-	const sm = createMap(s);
-	const tm = createMap(t);
+  const sm = createMap(s);
+  const tm = createMap(t);
 
-	return compareMaps(sm, tm);
+  return compareMaps(sm, tm);
 }
 
 function createMap(str) {
-	const sl = str.length;
-	const m = {};
+  const sl = str.length;
+  const m = {};
 
-	for (let i = 0; i < sl; i++) {
-		const s = str[i];
+  for (let i = 0; i < sl; i++) {
+    const s = str[i];
 
-		if (m[s]) {
-			m[s] += 1;
-			continue;
-		}
-		m[s] = 1;
-	}
-	return m;
+    if (m[s]) {
+      m[s] += 1;
+      continue;
+    }
+    m[s] = 1;
+  }
+  return m;
 }
 
 function compareMaps(m1, m2) {
-	for (let i in m1) {
-		if (m1[i] !== m2[i]) {
-			return false;
-		}
-	}
-	return true;
+  for (let i in m1) {
+    if (m1[i] !== m2[i]) {
+      return false;
+    }
+  }
+  return true;
 }
